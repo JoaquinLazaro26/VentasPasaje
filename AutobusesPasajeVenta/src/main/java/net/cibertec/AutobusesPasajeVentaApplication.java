c1c715050c045f7c12d41def8ecfd26cd8d44658
JoaquinLazaro <93229748+JoaquinLazaro26@users.noreply.github.com>
2023-05-06 16:30:50 -0500

    Create-Ventas Pasajes

---
+++ AutobusesPasajeVenta/src/main/java/net/cibertec/AutobusesPasajeVentaApplication.java
@@ -0,0 +1,13 @@
+package net.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobusesPasajeVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobusesPasajeVentaApplication.class, args);
+	}
+
+}
---
+++ AutobusesPasajeVenta/src/test/java/net/cibertec/AutobusesPasajeVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobusesPasajeVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
